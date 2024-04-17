ea9c0800d34dfb59bc8a9647208f0a0e0fc7174e
tyeon99 <xodus170@naver.com>
2024-04-17 14:06:24 +0900

    투자의 비책 퍼블리싱

---
+++ 투자의 비책/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'black' : '#141414',
+        'white' : '#fff',
+        'mainColor' : '#ff5050',
+      },},
+  },
+  plugins: [],
+}
+
